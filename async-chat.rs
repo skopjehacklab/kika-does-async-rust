422318a3dac2c82e157f2caee612f2aad409c12e
Дамјан Георгиевски <gdamjan@gmail.com>
2021-12-19 12:43:11 +0100

    cleanups, no timeout

--- async-chat.rs
+++ async-chat.rs
@@ -1,25 +1,21 @@
 #![allow(unused_variables)]
 
-use std::time::Duration;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net;
 use tokio::sync::broadcast;
-use tokio::time::timeout;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+    let (tx, _rx0) = broadcast::channel::<String>(100);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6667").await.unwrap();
-    println!("Listening on :6667");
-
-    let (tx, _rx0) = broadcast::channel::<String>(100);
+    println!("Listening on tcp://0.0.0.0:6667");
 
     loop {
         let (socket, addr) = listener.accept().await.unwrap();
         tokio::spawn(process_client(socket, addr, tx.clone()));
-        // do rx0
     }
 }
 
@@ -35,19 +31,19 @@ async fn process_client(mut socket: net::TcpStream, addr: std::net::SocketAddr, 
         let mut line = String::with_capacity(1024); // [u8]
 
         tokio::select! {
-            //                                   Future<Result<usize>>>
-            t = timeout(Duration::from_secs(60), reader.read_line(&mut line)) => {
-                let x = match t {
-                    Err(_) => break,
-                    Ok(Ok(0)) => break,
-                    Ok(Ok(1)) => break,
-                    Ok(Ok(_)) => { sender.send(line.clone()).unwrap(); continue },
-                    Ok(Err(e)) => break,
-                };
+
+            r = reader.read_line(&mut line) => match r {
+                Err(_) => break,
+                Ok(0) => break,
+                Ok(_) => {
+                    sender.send(format!("{}: {}", addr, line.clone())).unwrap();
+                    continue;
+                },
             },
 
-            Ok(msg) = receiver.recv() =>
-                writer.write_all(msg.as_bytes()).await.unwrap(),
+            Ok(msg) = receiver.recv() => {
+                writer.write_all(msg.as_bytes()).await.unwrap();
+            }
         }
     }
     println!("{} disconnected", addr);
